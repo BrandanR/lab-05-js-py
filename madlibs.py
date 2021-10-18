32f5d101ac45c1e5bef0b92fd7d041f03fdbaef8
Brandan Ramnandan <brandanramnandan@Brandans-Air.fios-router.home>
2021-10-18 17:53:30 -0400

    finished html and python updates

--- madlibs.py
+++ madlibs.py
@@ -1,10 +1,9 @@
-
-      var noun = 'school'
-      var adjective = 'fast'
-      var adverb = 'accidentally'
-      var verb = 'walked'
-      var noun = prompt("school" )
-      var adjective = prompt("fast" )
-      var adverb = prompt("accidentally" )
-      var verb= prompt("walked" )
-      console.log("On a school" + "Underneath a fast lava moon" + "accidentally with the hula dancers" + "walked questions cause they always got the answers" )
+    var noun = 'school';
+    var adjective = 'fast';
+    var adverb = 'accidentally';
+    var verb = 'walked';
+    var noun = prompt("school");
+    var adjective = prompt("fast");
+    var adverb = prompt("accidentally");
+    var verb = prompt("walked");
+    console.log("On a school" + "Underneath a fast lava moon" + "accidentally with the hula dancers" + "walked questions cause they always got the answers");
